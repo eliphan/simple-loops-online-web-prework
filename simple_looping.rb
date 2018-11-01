e19d2b7a361b05f48ed2227f6df7fc9d2554ca6a
generic-thread-7687 <aliceconnic@gmail.com>
2018-11-01 02:17:49 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,7 +11,7 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   loop do
     counter = 0
-    until counter = 7
+    while counter = 7
     puts "Welcome to Flatirowhile counter < 7
 end
 

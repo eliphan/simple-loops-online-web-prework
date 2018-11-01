ac6785e1c0edba4a5dd8fa8f17721f3eebb57277
generic-thread-7687 <aliceconnic@gmail.com>
2018-11-01 02:26:48 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,7 +11,9 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   loop do 
     puts "Welcome to Flatiron School's Web Development Course!"
-    if 
+    if number_of_times <= 7
+      break
+    end
 end
 
 def times_iterator(number_of_times)
